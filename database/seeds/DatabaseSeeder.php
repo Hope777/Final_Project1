77c54bbc81e5932439e26acc618908be351bd9c1
Victor <michel-v@ulster.ac.uk>
2018-05-11 19:26:57 +0100

    WW

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,31 +14,31 @@ class DatabaseSeeder extends Seeder {
 	{
     
 
- 		//$this->call(UserstableSeeder::class);
-        //$this->call(ChannelsTableSeeder::class);
+ 		$this->call(UserstableSeeder::class);
+        $this->call(ChannelsTableSeeder::class);
         $this->call(DiscussionsTableSeeder::class);
-        //$this->call(RepliesTableSeeder::class);
+        $this->call(RepliesTableSeeder::class);
 
-	    //$this->call(PackageTableSeeder::class);
-	    //$this->command->info('Package table seeded!');
+	    $this->call(PackageTableSeeder::class);
+	    $this->command->info('Package table seeded!');
 	    
-	    //$this->call(BookingDateTimeTableSeeder::class);
-	    //$this->command->info('Booking DateTimes seeded!');
+	    $this->call(BookingDateTimeTableSeeder::class);
+	    $this->command->info('Booking DateTimes seeded!');
 	    
-	    //$this->call(CustomerSeeder::class);
-	    //$this->command->info('Customers seeded!');
+	    $this->call(CustomerSeeder::class);
+	    $this->command->info('Customers seeded!');
 	    
-	    //$this->call(AppointmentSeeder::class);
-	    //$this->command->info('Appointments seeded!');
+	    $this->call(AppointmentSeeder::class);
+	    $this->command->info('Appointments seeded!');
 	    
-	    //$this->call(AdminSeeder::class);
-	    //$this->command->info('Admins seeded!');
+	    $this->call(AdminSeeder::class);
+	    $this->command->info('Admins seeded!');
 
-	    //$this->call(TimeIntervalTableSeeder::class);
-	    //$this->command->info('Time intervals seeded!');
+	   $this->call(TimeIntervalTableSeeder::class);
+	    $this->command->info('Time intervals seeded!');
 
-	    //$this->call(ConfigurationTableSeeder::class);
-	    //$this->command->info('Configurations seeded!');
+	    $this->call(ConfigurationTableSeeder::class);
+	    $this->command->info('Configurations seeded!');
 	    
 	    Eloquent::unguard();
 	}
